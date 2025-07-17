cb56dccd90ed4a6c3aeb5f0f461d48f706f10bf0
Volodymyr.Melnychuk <darktipzero@gmail.com>
2025-07-17 22:53:26 +0300

    Add status flags, addressing mode, opcodes declarations

--- nes-emulator-solution/CPU6502/CPU6502.cpp
+++ nes-emulator-solution/CPU6502/CPU6502.cpp
@@ -1,6 +1,6 @@
 ﻿#include "CPU6502.h"
 
-void CPU6502::reset()
+void CPU6502::resetInternal()
 {
     a_reg = 0x00;
     x_reg = 0x00;
@@ -10,11 +10,13 @@ void CPU6502::reset()
     status_reg = 0x00;
 
     fetch = 0x00;
+    cycles = 0;
+    cyclesCount = 0;
 }
 
 CPU6502::CPU6502()
 {
-    reset();
+    resetInternal();
 }
 
 CPU6502::~CPU6502() = default;
@@ -27,5 +29,22 @@ void CPU6502::write(uint16_t addr, uint8_t val)
 uint8_t CPU6502::read(uint16_t addr)
 {
     return bus->read(addr);
+}
+
+void CPU6502::setStatusFlag(StatusRegistryFlags flag, bool predicate)
+{
+    if (predicate)
+    {
+        status_reg |= flag;
+    }
+    else
+    {
+        status_reg &= ~flag;
+    }
+}
+
+uint8_t CPU6502::getStatusFlag(StatusRegistryFlags flag)
+{
+    return status_reg & flag;
 }
 
